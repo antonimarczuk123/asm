30e57d7e187a4a18773b1ad3fd7d89b8eff4ba40
Antoni Marczuk <antoni.marczuk123@gmail.com>
2024-03-09 18:52:15 +0100

    addressing

--- addresing.asm
+++ addresing.asm
@@ -1,6 +1,19 @@
+section .data
+var dd 0x12345678
+
 section .text
 global main
 main:
-    ;write your code here
-    xor rax, rax
+    mov rbp, rsp; for correct debugging
+
+    mov rax, 0
+    mov rbx, 0
+    mov rcx, 0
+    mov rdx, 0
+    
+    mov al, [var]
+    mov bl, [var+1]
+    mov cl, [var+2]
+    mov dl, [var+3]
+    
     ret
