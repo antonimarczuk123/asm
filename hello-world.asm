6245539f25dd2924e6c932ddedd5e351bf29434b
Antoni Marczuk <antoni.marczuk123@gmail.com>
2024-03-11 13:15:06 +0100

    hello-world

---
+++ hello-world.asm
@@ -0,0 +1,20 @@
+section .data
+    hello db 'Hello, World!', 0 
+    len equ $-hello
+    
+section .text
+    global _start
+    
+_start:
+    
+    ;write 'Hello, World!' to stdout
+    mov rax, 1      ;syscall number for sys_write
+    mov rdi, 1      ;file descriptor stdout
+    mov rsi, hello  ;address of text to display
+    mov rdx, len    ;string length
+    syscall
+    
+    ;exit the program
+    mov rax, 60     ;syscall number for sys_exit
+    xor rdi, rdi    ;return code 0
+    syscall
